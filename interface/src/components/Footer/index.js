d6fbf7a8a489dc527c7b515c6500c5cec24fdde2
chandn0 <boinapallychandan007@gmail.com>
2023-03-26 16:50:24 +0530

    UI

--- interface/src/components/Footer/index.js
+++ interface/src/components/Footer/index.js
@@ -1,15 +1,13 @@
 
 function Footer() {
     return (
-
-
-
         <footer class=" text-white shadow mt-4  dark:bg-gray-800">
             <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-white sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">SSC™</a>. All Rights Reserved.
-                </span>
+                {/* <span class="text-sm text-white sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">SSC™</a>. All Rights Reserved.
+                </span> */}
+                <h1>Made by <a href="https://twitter.com/chandan1_">Chandan</a></h1>
                 <ul class="flex flex-wrap items-center mt-3 text-sm text-white dark:text-gray-400 sm:mt-0">
-                    <li>
+                    {/* <li>
                         <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
                     </li>
                     <li>
@@ -17,9 +15,9 @@ function Footer() {
                     </li>
                     <li>
                         <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-                    </li>
+                    </li> */}
                     <li>
-                        <a href="#" class="hover:underline">Contact</a>
+                        <a href="https://github.com/Lavishq/average-USD" class="hover:underline">Review the code Github</a>
                     </li>
                 </ul>
             </div>
--- interface/src/pages/app/index.js
+++ interface/src/pages/app/index.js
@@ -211,7 +211,7 @@ export default function CoinApp() {
 
             <div>
               <div className='font-mono font-extrabold text-2xl text-center'>
-                <h1>Balance:{balance} VUSD</h1>
+                <h1>Your Balance:{balance} VUSD</h1>
 
               </div>
               {/* <div className='mt-5 font-mono font-bold text-lg text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div> */}
@@ -229,6 +229,9 @@ export default function CoinApp() {
                 <button onClick={BurnToken} type="button" class="text-white text-lg  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Burn VUSD</button>
               </div>
             </div>
+            <h1 >Currently the basket is only filled with DAI and USDC </h1>
+            <h1>The project is Currently on the mumbai testnet </h1>
+
 
 
 
