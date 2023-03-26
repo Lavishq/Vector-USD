77b82e6a201ee382b274ae44acc47194e129bec6
chandn0 <boinapallychandan007@gmail.com>
2023-03-26 20:15:00 +0530

    mint token

--- interface/src/pages/app/index.js
+++ interface/src/pages/app/index.js
@@ -48,6 +48,16 @@ export default function CoinApp() {
 
   }
 
+  const minttokens = async () => {
+    if (address) {
+      for (let index = 0; index < basket.length; index++) {
+        let contract = new ethers.Contract(basket[index]?.backedStableToken, ercabi, signer)
+        let amount = BigInt(100 * 10 ** (basket[index]?.decimal).toString())
+        let amount1 = contract.mint(address, amount)
+        console.log(amount1)
+      }
+    }
+  }
 
   const BuyToken = async () => {
     if (address) {
@@ -229,6 +239,7 @@ export default function CoinApp() {
                 <button onClick={BurnToken} type="button" class="text-white text-lg  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Burn VUSD</button>
               </div>
             </div>
+            <div onClick={minttokens}>Mint Tokens </div>
             <h1>Currently the smart contract is deployed in the Mumbai Testnet</h1>
             {/* <h1>Contract Address</h1>
             <h1> USDC Address </h1>
