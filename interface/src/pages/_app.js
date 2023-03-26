443ce73672ee3acaf6def327a431f8df8e02f6cd
Lavishq <v.whitehearted@simplelogin.co>
2023-03-26 19:17:08 +0530

    fixed error

--- interface/src/pages/_app.js
+++ interface/src/pages/_app.js
@@ -9,43 +9,6 @@ import { configureChains, createClient, goerli, WagmiConfig } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum, polygonMumbai, polygonZkEvmTestnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const Polygon_zkEVM = {
-  id: 1442,
-  name: 'Polygon zkEVM Testnet',
-  network: 'testnet',
-  iconUrl: 'https://example.com/icon.svg',
-  iconBackground: '#fff',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'zkEVM',
-    symbol: 'ETH',
-  },
-  rpcUrls: {
-    default: 'https://rpc.public.zkevm-test.net/',
-  },
-  blockExplorers: {
-    default: { name: 'zkevm', url: 'https://explorer.public.zkevm-test.net/' },
-    etherscan: { name: 'zkevm', url: 'https://explorer.public.zkevm-test.net/' },
-  },
-  testnet: true,
-};
-​
-const { provider, chains } = configureChains(
-  [Polygon_zkEVM],
-  [jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default }) })]
-);
-​
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  chains,
-});
-​
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-  // webSocketProvider,
-});
 
 
 const { chains, provider } = configureChains(
